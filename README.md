79cf20f383ba8819128619bfeeb9e90c2c3f7a80
Bruno | Gazeta comuna <contato@gazetacomuna.com.br>
2024-11-28 21:55:18 -0300

    feat: kind working. wip: add menus

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# gomde
+#  [wip] gomde (Go Markdown Editor) is a really simple markdown editor made with Golang and Fyne
--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
 type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	a := app.New()
+	win := a.NewWindow("GOMDE - Markdown")
+	edit, preview := cfg.makeUI()
+	win.SetContent(container.NewHSplit(edit, preview))
+	win.Resize(fyne.NewSize(400, 400))
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+func (cfg *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	cfg.EditWidget = edit
+	cfg.PreviewWidget = preview
+	// the right panel gonna show the edited version
+	edit.OnChanged = preview.ParseMarkdown
+	return edit, preview
 }
