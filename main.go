d4b06e794a0b4edd1e99cd5d76ae29967317ddca
Bruno | Gazeta comuna <contato@gazetacomuna.com.br>
2024-11-29 12:06:25 -0300

    NewFile saving 'save as' working and opening md files.  WIP: Save
     - next: rich text visual editor (like word)
     - Integration with marmite

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"io"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -19,19 +22,79 @@ var cfg config
 func main() {
 	a := app.New()
 	win := a.NewWindow("GOMDE - Markdown")
+
 	edit, preview := cfg.makeUI()
+	cfg.createMenuItems(win)
+
 	win.SetContent(container.NewHSplit(edit, preview))
 	win.Resize(fyne.NewSize(400, 400))
 	win.CenterOnScreen()
 	win.ShowAndRun()
 }
 
-func (cfg *config) makeUI() (*widget.Entry, *widget.RichText) {
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
 	edit := widget.NewMultiLineEntry()
 	preview := widget.NewRichTextFromMarkdown("")
-	cfg.EditWidget = edit
-	cfg.PreviewWidget = preview
+	app.EditWidget = edit
+	app.PreviewWidget = preview
 	// the right panel gonna show the edited version
 	edit.OnChanged = preview.ParseMarkdown
 	return edit, preview
+}
+
+func (app *config) createMenuItems(win fyne.Window) {
+	openMenuItem := fyne.NewMenuItem("Open menu", app.openFunc(win))
+	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+	menu := fyne.NewMainMenu(fileMenu)
+	win.SetMainMenu(menu)
+
+}
+func (app *config) openFunc(win fyne.Window) func() {
+	return func() {
+		openDialog := dialog.NewFileOpen(func(read fyne.URIReadCloser, err error) {
+			defer read.Close()
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+			if read == nil {
+				return
+			}
+			data, err := io.ReadAll(read)
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+			app.EditWidget.SetText(string(data))
+			app.CurrentFile = read.URI()
+			win.SetTitle(win.Title() + " - " + read.URI().Name())
+			app.SaveMenuItem.Disabled = false
+
+		}, win)
+		openDialog.Show()
+	}
+}
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+			if write == nil {
+				// user cancelou acao
+				return
+			}
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+			defer write.Close()
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+		}, win)
+		saveDialog.Show()
+	}
 }
